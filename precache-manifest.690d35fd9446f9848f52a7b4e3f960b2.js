a407dc2bccb1eaa808aeb7eaafb0b4201170e3e1
alexanderqchen <aqchen@ucla.edu>
2019-07-21 08:48:00 -0400

    Updates

---
+++ precache-manifest.690d35fd9446f9848f52a7b4e3f960b2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f5ee18bf61477be3e542e659fb4fc3b",
+    "url": "/todo-playground/index.html"
+  },
+  {
+    "revision": "b3d65b49000084f40dea",
+    "url": "/todo-playground/static/js/2.818d4288.chunk.js"
+  },
+  {
+    "revision": "5ed072f38f8cc8a8d65d",
+    "url": "/todo-playground/static/js/main.da0d6e0f.chunk.js"
+  },
+  {
+    "revision": "6d641122e7b86fd34bc5",
+    "url": "/todo-playground/static/js/runtime~main.66366e5a.js"
+  }
+]);
